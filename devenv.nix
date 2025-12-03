9335bf18d2be7d08418e55bfb51219fef7170281
Luiz Ribeiro <luizribeiro@gmail.com>
2025-12-02 19:47:49 -0500

    Add pre-commit hooks and apply rustfmt
    - Configure git-hooks in devenv.nix for rustfmt, clippy, cargo-check
    - Apply rustfmt formatting to all Rust files

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   languages.rust.enable = true;
+
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    clippy = {
+      enable = true;
+      settings.allFeatures = true;
+    };
+    cargo-check.enable = true;
+  };
 }
