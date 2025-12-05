450452f07c9a13a39eccc94b3d1fc83604917605
Luiz Ribeiro <luizribeiro@gmail.com>
2025-12-05 11:57:57 -0500

    Add LiveViewDisplayEffect and ShutterModeStatus enums
    - LiveViewDisplayEffect: Unknown/On/Off for live view display settings
    - ShutterModeStatus: Off/Speed/Angle/ECS/Auto for cinema camera shutter modes
    
    Both properties now display human-readable values instead of raw integers.

--- crsdk/src/lib.rs
+++ crsdk/src/lib.rs
@@ -77,7 +77,8 @@ pub use event::{warning_code_name, warning_param_description, CameraEvent};
 pub use property::{
     format_movie_quality, AspectRatio, AutoManual, DataType, DeviceProperty, DriveMode, EnableFlag,
     ExposureProgram, FileType, FlashMode, FocusArea, FocusMode, ImageQuality, ImageSize,
-    MeteringMode, MovieFileFormat, OnOff, PropertyCode, Switch, WhiteBalance,
+    LiveViewDisplayEffect, MeteringMode, MovieFileFormat, OnOff, PropertyCode, ShutterModeStatus,
+    Switch, WhiteBalance,
 };
 pub use sdk::Sdk;
 pub use types::{CameraModel, ConnectionInfo, ConnectionType, DiscoveredCamera, MacAddr};
