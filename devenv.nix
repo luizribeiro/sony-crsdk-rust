16a7db06a4d6acf6a0b414f8c609d8bf46d6f07b
Luiz Ribeiro <luizribeiro@gmail.com>
2025-12-08 14:18:56 -0500

    Make sonyctl available directly in devenv shell
    Build sonyctl on shell entry and add target/debug to PATH so the binary
    can be invoked directly as 'sonyctl' without 'cargo run'.

--- devenv.nix
+++ devenv.nix
@@ -5,6 +5,11 @@
 
   languages.rust.enable = true;
 
+  enterShell = ''
+    cargo build -p sonyctl --quiet 2>/dev/null || true
+    export PATH="$DEVENV_ROOT/target/debug:$PATH"
+  '';
+
   git-hooks.hooks = {
     rustfmt.enable = true;
     clippy = {
